b076e8221f76aeeaf04f9d286a389fe231cc8199
Terran Kartik Tellus <91951646+TerranKartikTellus@users.noreply.github.com>
2023-04-28 19:17:47 +0530

    head fix

--- components/head.js
+++ components/head.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 
 export default function Headd({title}){
-
+return (
   <Head>
         <title>{title}</title>
         <meta name="description" content="This is a description of my page." />
         <link rel="icon" href="/favicon.ico" />
         {/* <script src="/my-script.js"></script> */}
       </Head>
-}
+)}
